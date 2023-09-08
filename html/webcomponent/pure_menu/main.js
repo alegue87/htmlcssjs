4299f64c778aa1405452662d578b2e9106dc46ba
alessio <alegue87@gmail.com>
2023-09-08 14:27:10 +0200

    Inserito button per ogni leaf

--- html/webcomponent/pure_menu/main.js
+++ html/webcomponent/pure_menu/main.js
@@ -5,45 +5,64 @@ class Branch extends HTMLUListElement{
         super()
     }
 }
+class LeafButton extends HTMLButtonElement {
+    constructor(){
+        super()
+    }
+    setName(name){
+        this.textContent = name
+    }
+}
 
 class Leaf extends HTMLLIElement {
+    name = ''
+    leafButton = null
+
     constructor(){
         super()
     }
     
     /**
      * Initialize HTMLLIElement Leaf
-     * @param {String} name - Name of leaf in tree
-     * @param {Boolean} addState - Initial state of leaf ( if is set is 'hidden' )
+     * @param {String} name- Name of leaf in tree
+     * @param {Boolean} hide - default is true
      * 
      */
-    init(name, addState=true){
-        this.textContent = name
-        if(addState){
+    setName(name, hide=true){
+        //this.textContent = name
+        if(hide){
             this.classList.add('hide')
         }
+        this.leafButton = new LeafButton()
+        this.leafButton.setName(name)
+        this.appendChild(this.leafButton)
     }
 
     connectedCallback(){
-        this.addEventListener('click', this.handleClick)
+        //this.addEventListener('click', this.handleClick)
+        // viene passato l'oggetto chiamante a leafButton ( leaf )
+        this.leafButton.that = this
+        this.leafButton.addEventListener('click', this.handleClick)
     }
 
     handleClick(e){
-        const childsLeaf = this.querySelector('ul').children
+        const branch = this.that.querySelector('ul')
+        if(!branch) return
+
+        const childsLeaf = branch.children
 
         for( let i = 0 ; i < childsLeaf.length ; i++ ){
-            const leafClasses = childsLeaf.item(i).classList
+            const leaf = childsLeaf.item(i)
             
-            leafClasses.toggle('hide')
-            console.log(childsLeaf.item(i))
+            leaf.classList.toggle('hide')
+            console.log(leaf)
         }
-        
     }
 }
 
 customElements.define('p-leaf', Leaf, { extends: 'li' })
 customElements.define('p-branch', Branch, { extends: 'ul' })
-
+customElements.define('p-leaf-button', LeafButton, { extends: 'button' })
 
 
 class Tree extends HTMLElement {
@@ -63,21 +82,31 @@ class Tree extends HTMLElement {
         
         const root = this.shadowRoot.querySelector("ul");
 
-        let plantsLeaf = new Leaf()
-        var addState;
-        plantsLeaf.init('Plants', addState=false)
+        const plantsLeaf = new Leaf()
+        var hide
+        plantsLeaf.setName('Plants', hide=false)
         //let animals = new Leaf().init('Animals')
 
         let plantsBranch = new Branch()
         plantsLeaf.appendChild(plantsBranch)
 
         const roseLeaf = new Leaf()
-        roseLeaf.init('Rose')
+        roseLeaf.setName('Rose')
+        const roseBranch = new Branch()
+        const rose1 = new Leaf()
+        rose1.setName `rose 1` // alternativa 
+        const rose2 = new Leaf()
+        rose2.setName('rose 2')
+        roseBranch.append(rose1)
+        roseBranch.append(rose2)
+        roseLeaf.append(roseBranch)
+
         const tulipLeaf = new Leaf()
-        tulipLeaf.init('Tulip')
+        tulipLeaf.setName('Tulip')
         
         plantsBranch.appendChild(roseLeaf)
         plantsBranch.appendChild(tulipLeaf)
+
 
         root.appendChild(plantsLeaf)
         
